8a3ea544d72a8a3dca2de24ef4effaa768173122
Lucas W. Schuch <lucas.schuch@gmail.com>
2025-07-05 21:44:57 -0300

    Remove unused files

--- test-imports.js
+++
@@ -1,6 +0,0 @@
-// Teste de importação para verificar se há problemas
-import StockMovementEditForm from "./app/stock-movement/[id]/StockMovementEditForm";
-import InvoiceEditForm from "./app/invoices/[id]/InvoiceEditForm";
-
-console.log("StockMovementEditForm:", StockMovementEditForm);
-console.log("InvoiceEditForm:", InvoiceEditForm);
--- types/tableData.ts
+++
@@ -1,7 +0,0 @@
-export type TableData = {
-  id: number;
-  col1: number | string;
-  col2?: number | string;
-  col3?: number | string;
-  col4?: number | string;
-};
